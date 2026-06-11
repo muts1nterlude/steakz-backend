38c919a854b00f5d3959228bab042dd0e8797d6f
muts1nterlude <mutsawavic@gmail.com>
2026-06-11 16:25:53 +0200

    fix: allow patch method in cors

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const allowedOrigins = (process.env['FRONTEND_URL'] ?? 'http://localhost:5173')
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
